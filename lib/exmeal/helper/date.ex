aff5d23cd184112196bd4c7fed3a550a0d8b917e
Victor Moraes de Aragõ <vct.aragao@gmail.com>
2022-01-29 00:05:07 -0300

    Helper para tratar com datas

---
+++ lib/exmeal/helper/date.ex
@@ -0,0 +1,35 @@
+defmodule Exmeal.Helper.Date do
+  @spec format_datetime(String.t(), String.t(), String.t()) ::
+          {:error,
+           :incompatible_calendars
+           | :invalid_date
+           | :invalid_format
+           | :invalid_time
+           | :missing_offset}
+          | {:ok, DateTime.t(), integer}
+  def format_datetime(date, hour, minute) do
+    date = format_date(date)
+    time = format_time(hour, minute)
+
+    DateTime.from_iso8601("#{date}T#{time}Z")
+  end
+
+  @spec format_date(String.t()) :: String.t()
+  def format_date(date) do
+    date
+    |> String.split("/")
+    |> Enum.reverse()
+    |> Enum.join("-")
+  end
+
+  @spec format_time(String.t(), String.t(), String.t()) :: String.t()
+  def format_time(hour, minute, seconds \\ "00") do
+    hour = pad_time(hour)
+    minute = pad_time(minute)
+    seconds = pad_time(seconds)
+
+    "#{hour}:#{minute}:#{seconds}"
+  end
+
+  defp pad_time(time), do: String.pad_leading(time, 2, "00")
+end
