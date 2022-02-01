80bf41999f68edf6e4949fa8d57a7c5f15a37071
Victor Moraes de Aragõ <vct.aragao@gmail.com>
2022-02-01 20:41:00 -0300

    Teste para criar Meal pela fachada

---
+++ test/exmeal_web/exmeal_test.exs
@@ -0,0 +1,34 @@
+defmodule ExmealWeb.Test.ExmealTest do
+  use Exmeal.DataCase, async: true
+
+  alias Exmeal.Meal.CreateMeal
+  alias Exmeal.Meal
+
+  setup_all do
+    params = %{description: "descrição", date: DateTime.utc_now(), calories: 100.50}
+    {:ok, params: params}
+  end
+
+  describe "create_meal/1" do
+    test "Given a map o valid params receive a Meal", setup do
+      assert {:ok, %Meal{}} = CreateMeal.call(setup[:params])
+    end
+
+    test "Given a map with invalid params receive an error", setup do
+      params =
+        setup[:params]
+        |> Map.delete(:description)
+        |> Map.put(:desc, "descrição")
+
+      assert {:error, "Can't register meal", [reason: "invalid params"]} ==
+               CreateMeal.call(params)
+    end
+
+    test "Giving not a map reacive an error", _setup do
+      assert {:error, "Can't register meal", [reason: "invalid params"]} ==
+               CreateMeal.call(
+                 {:description, "descrição", :date, DateTime.utc_now(), :calories, 100.50}
+               )
+    end
+  end
+end
