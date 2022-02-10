952ef5e71adad166debe155f14c35548f7440be3
Victor Moraes de Aragõ <vct.aragao@gmail.com>
2022-02-09 22:09:18 -0300

    Model para retornar uma meal

---
+++ lib/exmeal/meal/get_meal.ex
@@ -0,0 +1,13 @@
+defmodule Exmeal.GetMeal do
+  alias Exmeal.Repo
+  alias Exmeal.Meal
+
+  def call(%{id: id}) when is_integer(id), do: get_meal(id) |> handle_result()
+
+  def call(_), do: {:error, "Invalid params"}
+
+  defp get_meal(id), do: Repo.get(Meal, id)
+
+  defp handle_result(%Meal{} = meal), do: {:ok, meal}
+  defp handle_result(nil), do: {:error, "id is invalid"}
+end
